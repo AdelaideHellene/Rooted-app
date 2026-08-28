56733a3cfd5d2be3f1b942d381cbeee9dca9ccb7
Adelaide Hellene <adel.hellene@icloud.com>
2026-08-28 13:37:41 +0400

    Firebase connection

---
+++ Rooted code/js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2sfH1cQv6H8LCKzbUawhQg_itA071lQU",
+    authDomain: "rooted-4b15b.firebaseapp.com",
+    projectId: "rooted-4b15b",
+    storageBucket: "rooted-4b15b.firebasestorage.app",
+    messagingSenderId: "983787619185",
+    appId: "1:983787619185:web:ede50dc4bea9d97527acd0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
--- Rooted code/js/splash.js
+++ Rooted code/js/splash.js
@@ -1,8 +1,11 @@
-const startButton = document.querySelector("#start-button");
+import { app } from "./firebase.js";
 
+const startButton = document.querySelector("#start-button");
 
 startButton.addEventListener("click", function(){
 
     window.location.href = "welcome.html";
 
 });
+
+console.log("🌱 Firebase connected!", app);
